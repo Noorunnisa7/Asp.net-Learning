27a76c74da013cda46473066a300c20f4a0d272a
noorunnisa7 <noorunnisa560@gmail.com>
2025-10-30 05:58:48 -0700

    Controller and Razor View

---
+++ Class2/Clas2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clas2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.data = "Welcometo my Asp.net Website";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Class2/Clas2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Clas2.MvcApplication" Language="C#" %>
