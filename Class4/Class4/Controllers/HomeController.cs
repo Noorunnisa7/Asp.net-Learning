2634bca45ff3552ea43f342226f7c2a6de5ada46
noorunnisa7 <shazaibkhan@gmail.com>
2025-11-04 04:52:22 -0800

    View bag,view data and temp data

---
+++ Class4/Class4/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Class4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewBag.data = "View Bag Method";
+            ViewData["msg"] = "View data object";
+            TempData["Message"] = "TEMp data object";
+            return RedirectToAction("About");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            TempData.Keep("Message");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
