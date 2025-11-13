fd3a3488db3119a1ba111f47b5b7e26575f9840a
noorunnisa7 <shazaibkhan@gmail.com>
2025-11-13 04:53:19 -0800

    Models

---
+++ Class6/Class6/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Class6.Models;
+
+namespace Class6.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            //var student = new Student {Id = 1 , Name = "Ali" , age = 24};
+            var students = new List<Student>{
+                new Student { Id = 1, Name = "Ali", age = 24 },
+                 new Student { Id = 2, Name = "Raza", age = 25 },
+                  new Student { Id = 3, Name = "Hassan", age = 26 },
+                   new Student { Id = 4, Name = "Haider", age = 23 }
+            };
+            return View(students);
+        }
+    }
+}
