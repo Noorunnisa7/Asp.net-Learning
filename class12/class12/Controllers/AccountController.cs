034c98d5433ff262d3ac729da18330e7507087bb
noorunnisa7 <noorunnisa560@GMAIL.COM>
2025-12-02 18:05:12 +0500

    Login with session and cookie

---
+++ class12/class12/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using class12.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace class12.Controllers
+{
+    public class AccountController : Controller
+    {
+        public List<LoginViewModel> users = new List<LoginViewModel> { 
+          new LoginViewModel{username = "admin" , password = "12345" }
+        };
+
+
+
+        //get
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //post
+        [HttpPost]
+        public IActionResult Index(string username , string password)
+        {
+
+            var user = users.FirstOrDefault(x => x.username == username && x.password == password);
+
+            if (user == null)
+            {
+                return View("Index");
+            }
+            HttpContext.Session.SetString("username", username);
+
+            return RedirectToAction("Dashboard" , "Home");
+
+            
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
